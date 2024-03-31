20c48949ba360204b9920aa124d2094a9be4f39d
Sameh <sameh.abdelhaleem95@gmail.com>
2024-04-01 00:16:47 +0200

    refactor getPostsFiles to use it in dynamic post details component

--- lib/posts-util.jsx
+++ lib/posts-util.jsx
@@ -2,11 +2,14 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 const postsDirectory = path.join(process.cwd(), "posts");
-function getPostData(fileName) {
-  const filePath = path.join(postsDirectory, fileName);
+export function getPostsFiles() {
+  return fs.readdirSync(postsDirectory);
+}
+export function getPostData(postIdentifier) {
+  const postSlug = postIdentifier.replace(/\.md$/, ""); // remove file extension
+  const filePath = path.join(postsDirectory, `${postSlug}.md`);
   const fileContent = fs.readFileSync(filePath, "utf-8");
   const { data, content } = matter(fileContent);
-  const postSlug = fileName.replace(/\.md$/, ""); // remove file extension
   const postData = {
     slug: postSlug,
     ...data,
@@ -15,8 +18,7 @@ function getPostData(fileName) {
   return postData;
 }
 export function getAllPosts() {
-  const postFiles = fs.readdirSync(postsDirectory);
-
+  const postFiles = getPostsFiles();
   const allPosts = postFiles.map((postFile) => {
     return getPostData(postFile);
   });
