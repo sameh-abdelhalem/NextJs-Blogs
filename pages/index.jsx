0ff11c37df6e076a6ad5b7b6652ee36f75e9b983
Sameh <sameh.abdelhaleem95@gmail.com>
2024-03-31 20:58:08 +0200

    add post-grid and post-item components to render featured posts

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
+import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
-      {/* <FeaturedPosts /> */}
+      <FeaturedPosts />
     </>
   );
 }
