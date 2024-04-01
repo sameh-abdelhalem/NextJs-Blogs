76c5d34d42b757a570151705c4955807c94a4f85
Sameh <sameh.abdelhaleem95@gmail.com>
2024-04-01 02:04:59 +0200

    send comments to mongodb

--- pages/api/contact.jsx
+++ pages/api/contact.jsx
@@ -1,4 +1,5 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (
@@ -18,7 +19,26 @@ export default function handler(req, res) {
       name,
       message,
     };
-    console.log(newMessage);
+    let client;
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://samehabdelhaleem95:YuSblJrvxIQiUuqU@cluster0.5ngd1qg.mongodb.net/my-blog?retryWrites=true&w=majority&appName=Cluster0"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database" });
+      return;
+    }
+    const db = client.db();
+    let result;
+    try {
+      result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "storing message failed!" });
+      return;
+    }
+    client.close();
     res
       .status(201)
       .json({ message: "Success stored message", message: newMessage });
